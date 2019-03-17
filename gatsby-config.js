23f2c9cddf510bf56f4fa819c8ecd14c35776af0
Anton Holmberg <anton.holmberg@greenely.se>
2019-03-17 13:20:29 +0100

    Should say helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
       },
     },
     'gatsby-plugin-catch-links',
-    'gatsby-plugin-react-helm',
+    'gatsby-plugin-react-helmet',
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
