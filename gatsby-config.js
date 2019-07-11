0d6842f0c5a5a51b8c22b178ff7390b23d1b6c89
antonholmberg <anton.holmberg@protonmail.com>
2019-07-11 12:59:44 +0200

    Add language to html tag

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: 'gatsby-plugin-html-attributes',
+      options: {
+        lang: 'en',
+      },
+    },
+    {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'posts',
